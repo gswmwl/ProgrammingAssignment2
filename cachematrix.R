e2d6398732abf1b3ed5bc0b351a661c9105b8e62
gswmwl <34221440+gswmwl@users.noreply.github.com>
2018-04-14 08:48:27 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(m = matrix()){
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialize the inverse property
+    i <-  NULL
+Set <- function( matrix ) {
+            m <<-matrix
+            i <<- NULL
+    }
+    ## Method the get the matrix
+    get <- function() {
+      ##  Return the matrix
+      m
+    }
+## Way to set the inverse of the matrix
+    setInverse  <-  function(inverse) {
+        i <<- inverse
+    }
+    
+    ## Way to set the inverse of the matrix
+    getInverse <- function()  {
+        ## Back the inverse property
+        i
+    }
+## Back a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Compute the inverse of the unique matrix back by "makeCacheMatrix"
+## Back to a matrix   "m"
+    m <-  x$getInverse()
+    if( !is.null(m) )  {
+            message("getting cached data")
+            return(m)
+    }
+    ## Compute the inverse via matrix multiplication
+    m <- solve(data) %*% data
+## Set the inverse to the object
+    x$setInverse(m)
+## Coming back the matrix
+    m
 }
